e1c8f243e2fc3dec24eab74215751279289a0307
pooja-sree <35067811+pooja-sree@users.noreply.github.com>
2024-07-30 16:30:03 +0530

    Add files via upload

--- Open.java
+++ Open.java
@@ -1,26 +1,13 @@
 package selenium;
 
-
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-
+import org.openqa.selenium.*;
 public class Open {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		System.setProperty("Webdriver.chrome.driver", "C:\\Program Files\\chromedriver-win64\\chromedriver.exe");
-		
-		WebDriver driver = new ChromeDriver();
-		driver.get("https://www.leafground.com/link.xhtml");
-		
-		//Links
-//		driver.findElement(By.linkText("Go to Dashboard")).click();
-//		
-//		driver.findElement(By.partialLinkText("without clicking me.")).click();
-		
-		
 
+		System.setProperty("Webdriver.chrome.driver", "C:\\Program Files\\chromedriver-win64\\chromedriver.exe");
+		webdriver driver = new chromedriver();
 	}
 
 }
